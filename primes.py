67efc0925c63ee518db206b0d27b2125872962b9
Miguel A G Valverde <miguelg@usp.br>
2017-07-28 12:56:35 -0300

    fixing bug when a prime is given

--- primes.py
+++ primes.py
@@ -4,6 +4,7 @@ def get_primes(n):
     for i in range(3,math.ceil(math.sqrt(n))+1,2):    
         if n%i==0:
             primes.append(i)
+    #print (primes)
     return primes
 
 def get_prime_factors(n):
@@ -11,6 +12,9 @@ def get_prime_factors(n):
     i=0
     factors =[]
     while n>1:
+        if i>=len(primes):
+            factors.append(int(n))
+            break
         if n%primes[i]==0:
             factors.append(primes[i])
             n/=primes[i]
@@ -18,5 +22,6 @@ def get_prime_factors(n):
             i+=1;
     return factors
 
-test_value=20
-print("*".join(map(str,get_prime_factors(test_value))))
+test_value=10002308
+for test_value in range(2,100):
+    print("para ",test_value," o resultado é ","*".join(map(str,get_prime_factors(test_value))))
